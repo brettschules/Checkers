4df20c252d26c67c2ea0cdb578f677a15baea4d3
Brett Schules <brettschules@gmail.com>
2017-06-19 17:12:44 -0400

    created board and cell classes

---
+++ game.js
@@ -0,0 +1,29 @@
+class Cell() {
+  constructor(x,y) {
+    this.x = x
+    this.y = y
+  }
+}
+
+class Board() {
+  constructor() {
+
+    this.board = [
+                    [new Cell(00, new Cell(10),new Cell(20),new Cell(30),new Cell(40),new Cell(50),new Cell(60),new Cell(70)],
+                    [new Cell(01, new Cell(11),new Cell(21),new Cell(31),new Cell(41),new Cell(51),new Cell(61),new Cell(71)],
+                    [new Cell(02, new Cell(12),new Cell(22),new Cell(32),new Cell(42),new Cell(52),new Cell(62),new Cell(72)],
+                    [new Cell(03, new Cell(13),new Cell(23),new Cell(33),new Cell(43),new Cell(53),new Cell(63),new Cell(73)],
+                    [new Cell(04, new Cell(14),new Cell(24),new Cell(34),new Cell(44),new Cell(54),new Cell(64),new Cell(74)],
+                    [new Cell(05, new Cell(15),new Cell(25),new Cell(35),new Cell(45),new Cell(55),new Cell(65),new Cell(75)],
+                    [new Cell(06, new Cell(16),new Cell(26),new Cell(36),new Cell(46),new Cell(56),new Cell(66),new Cell(76)],
+                    [new Cell(06, new Cell(17),new Cell(27),new Cell(37),new Cell(47),new Cell(57),new Cell(67),new Cell(77)],
+
+
+
+
+
+
+
+                                                                                                                                ]
+  }
+}
