e959f9c8d78cc0fdd7cb9ed8b05d103d2d76ebe1
Brett Schules <brettschules@gmail.com>
2017-06-21 16:16:32 -0400

    Added simple rules

--- game.js
+++ game.js
@@ -1,138 +1,169 @@
 class ChessPiece {
-  constructor(player) {
+  constructor(player,cell) {
     this.player = player
-    this.ChessPiece = this.generateChessPiece()
-  }
-
-  generateChessPiece() {
-    this.color = ""
-
-    if (this.player.color === "red") {
-      // needs to be this.player.color when not debugging
-      this.color = "red"
-    } else if (this.player.color === "grey") {
-      // needs to be this.player.color when not debugging
-      this.color = "grey"
-    }
-    return this.color
-
-  }
-
-  displayChessPieces() {
-    this.div = ``
-    if (this.generateChessPiece() === "red") {
-      this.div = `<div class='piece-${this.generateChessPiece()}'> </div>`
-
-    } else if (this.generateChessPiece() === "grey") {
-      this.div = `<div class='piece-${this.generateChessPiece()}'> </div>`
-    }
-    return this.div
+    this.cell = cell
   }
 
   render() {
-    return this.displayChessPieces()
+    return `<div class='piece-${this.player.color}'></div>`
   }
 }
 
-  class Cell {
-    constructor(x, y, color) {
-      this.x = x,
-      this.y = y,
-      this.color = color
-      this.piece = null
-    }
+class Cell {
+  constructor(x, y, color) {
+    this.x = x,
+    this.y = y,
+    this.color = color
+    this.piece = null
+  }
 
-    addPiece(piece) {
-      this.piece = piece
+  getCoordinates(clicked) {
+    console.log(clicked)
+    this.x = x
+    this.y = y
+  }
 
-    }
+  addPiece(piece) {
+    this.piece = piece
 
-    render() {
-      return (
-        `<div data-x='${this.x}' data-y='${this.y}'class='cell cell-${this.color}'>
+  }
+
+  render() {
+    return (
+      `<div data-x='${this.x}' data-y='${this.y}'class='cell cell-${this.color}'>
           ${this.piece !== null ? this.piece.render() : ""}
         </div>`
+    )
+  }
+}
 
-
-
-      )
-    }
+class Board {
+  constructor() {
+    this.boardSize = 8
+    this.board = this.generateBoard()
   }
 
-  class Board {
-    constructor() {
-      this.boardSize = 8
-      this.board = this.generateBoard()
-    }
+  generateBoard() {
+    let colors = ['white', 'black']
+    var boardArray = []
 
-    generateBoard() {
-      let colors = ['white', 'black']
-      var boardArray = []
-
-      for (let x = 0; x < this.boardSize; x++) {
-        let colorNum = 0
-        if (x % 2 === 0) {
-          colorNum = 0
-        } else {
-          colorNum = 1
-        }
-        let rowArray = []
-        for (let y = 0; y < this.boardSize; y++) {
-          colorNum === 1 ? colorNum = 0 : colorNum = 1
-          rowArray.push(new Cell(x, y, colors[colorNum]))
-        }
-        boardArray.push(rowArray)
+    for (let x = 0; x < this.boardSize; x++) {
+      let colorNum = 0
+      if (x % 2 === 0) {
+        colorNum = 0
+      } else {
+        colorNum = 1
       }
-      return boardArray
+      let rowArray = []
+      for (let y = 0; y < this.boardSize; y++) {
+        colorNum === 1 ? colorNum = 0 : colorNum = 1
+        rowArray.push(new Cell(x, y, colors[colorNum]))
+      }
+      boardArray.push(rowArray)
     }
-
-    render() {
-      return (
-        `<div class='board'>${this.board.map( row => row.map( cell => cell.render() ).join('') ).join('')}</div>`
-      )
-    }
+    return boardArray
   }
 
-  class Player {
-    constructor(color) {
-      this.color = color
-      return this.color
-    }
+  render() {
+    return (
+      `<div class='board'>${this.board.map( row => row.map( cell => cell.render() ).join('') ).join('')}</div>`
+    )
+  }
+}
+
+class Player {
+  constructor(color) {
+    this.color = color
+    return this.color
+  }
+}
+
+class Game {
+  constructor() {
+    this.playerOne = new Player("red")
+    this.playerTwo = new Player("grey")
+    this.boardObj = new Board()
+    this.board = this.boardObj.board
+    this.loadPieces = this.loadPieces()
   }
 
-  class Game {
-    constructor() {
-      this.playerOne = new Player("red")
-      this.playerTwo = new Player("grey")
-      this.board = new Board()
-      this.loadPieces = this.loadPieces()
-    }
 
 
+  loadPieces() {
+    this.array = []
 
-    loadPieces() {
-      this.array = []
+    this.board.forEach(row => this.array.push(row.filter(e => e.color === "black")))
+    this.first12 = this.array.slice(0, 3)
+    this.last12 = this.array.slice(this.array.length - 3)
+    this.first12.forEach(rowArray => rowArray.forEach(cell => cell.addPiece(new ChessPiece(this.playerOne,cell))))
+    this.last12.forEach(rowArray => rowArray.forEach(cell => cell.addPiece(new ChessPiece(this.playerTwo,cell))))
+  }
+}
 
-      this.board.board.forEach(row => this.array.push(row.filter(e => e.color === "black")))
-      this.first12 = this.array.slice(0, 3)
-      this.last12 = this.array.slice(this.array.length - 3)
-      this.first12.forEach(rowArray => rowArray.forEach(cell => cell.addPiece(new ChessPiece(this.playerOne))))
-      this.last12.forEach(rowArray => rowArray.forEach(cell => cell.addPiece(new ChessPiece(this.playerTwo))))
-    }
+class CheckerRules {
+  constructor() {
 
-    movePieces() {
-
-    }
   }
 
-  class App {
-    constructor() {
-      this.game = new Game()
+  validMove(destination,origin) {
+    let currentPlayerChessPieceColor = origin.piece.player.color
+
+    if (destination.x < 0 || destination.y < 0) {
+      return false
     }
-    render() {
-      document.getElementById('container').innerHTML = this.game.board.render()
+
+    if (destination.piece !== null) {
+      return false
+    }
+
+    if (destination.piece !== null && currentPlayerChessPieceColor === destination.piece.player.color) {
+      return false
+    }
+
+    switch(currentPlayerChessPieceColor) {
+    case "grey":
+
+
+        if (origin.x - 1 === destination.x && origin.y - 1  === destination.y) {
+          return true
+        } else if (origin.x -1 === destination.x &&  origin.y + 1 === destination.y) {
+          return true
+        } else if (true) {
+
+        };
+    case "red":
+      if (origin.x + 1 === destination.x && origin.y - 1 === destination.y) {
+        return true
+      } else if (origin.x + 1 === destination.x && origin.y + 1 === destination.y) {
+        return true
+      } else if (true) {
+
+      }
+
+
+    default:
+        return false
     }
   }
+}
 
-  checkers = new App()
-  checkers.render()
+class App {
+  constructor() {
+    this.game = new Game()
+    this.game.rules = new CheckerRules
+    this.boardContainer = document.getElementById("container")
+    //this.boardContainer.addEventListener("click", this.onClick.bind(this))
+  }
+
+  onClick() {
+    const clickedEvent = event.target
+    this.getCoordinates(clickedEvent)
+  }
+
+  render() {
+    document.getElementById('container').innerHTML = this.game.boardObj.render()
+  }
+}
+
+checkers = new App()
+checkers.render()
